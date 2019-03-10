4a7a8636b6172f363d3485b8a6d028e7816b4155
Philippe <phildoreau45@gmail.com>
2019-03-10 22:32:53 +0100

    first commit

---
+++ src/app/lib/common/input-ref.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, HostListener } from "@angular/core";
+
+@Directive({
+  selector: "au-fa-input input"
+})
+export class InputRefDirective {
+  constructor() {}
+
+  focus = false;
+
+  @HostListener("focus")
+  onFocus() {
+    this.focus = true;
+  }
+  @HostListener("blur")
+  onBlur() {
+    this.focus = false;
+  }
+}
